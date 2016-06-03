70750a44df7e3bf5036704d3319efd63df017f15
伍峰杰 <wufengjie@u51.com>
2016-06-03 17:14:07 +0800

    update

--- src/app/animate.js
+++ src/app/animate.js
@@ -6,12 +6,19 @@ const expectedDuration = 350;
 
 export const animateForward = function(oldWebview, newWebview, cb){
 
+    // 准备 变换
     newWebview.$node.style.zIndex = 1000;
     oldWebview.$node.style.zIndex = 999;
+
+    newWebview.$node.style.visibility = "visible";
+    oldWebview.$node.style.visibility = "visible";
 
     newWebview.$node.style.transition="none";
     addClass(newWebview,'mx-webview-forward');
 
+
+
+    //开始 变换
     setTimeout(function(){
         newWebview.$node.style.transition="";
         addClass(oldWebview,'mx-webview-backward');
@@ -20,12 +27,15 @@ export const animateForward = function(oldWebview, newWebview, cb){
 
     },1000/60);
 
-    onTransitionEnd(newWebview.$node, expectedDuration, function(){
 
+    //结束 变换
+    onTransitionEnd(newWebview.$node, expectedDuration, function(){
         addClass(newWebview,'active');
         removeClass(oldWebview, 'mx-webview-backward');
         newWebview.$node.style.zIndex = "";
         oldWebview.$node.style.zIndex = "";
+        newWebview.$node.style.visibility = "";
+        oldWebview.$node.style.visibility = "";
         cb();
     });
 
@@ -33,27 +43,34 @@ export const animateForward = function(oldWebview, newWebview, cb){
 
 export const animateBackward = function(oldWebview, newWebview, cb){
 
+    //准备 变换
     newWebview.$node.style.zIndex = 999;
     oldWebview.$node.style.zIndex = 1000;
+
+    newWebview.$node.style.visibility = "visible";
+    oldWebview.$node.style.visibility = "visible";
 
     newWebview.$node.style.transition="none";
     addClass(newWebview,'mx-webview-backward');
 
+    //开始 变换
     setTimeout(function(){
         newWebview.$node.style.transition="";
         removeClass(newWebview,'mx-webview-backward');
         addClass(oldWebview,'mx-webview-forward');
     },1000/60);
 
+    //结束 变换
     onTransitionEnd(newWebview.$node, expectedDuration, function(){
         addClass(newWebview,'active');
         removeClass(oldWebview, 'mx-webview-forward');
         removeClass(oldWebview, 'active');
         newWebview.$node.style.zIndex = "";
         oldWebview.$node.style.zIndex = "";
+        newWebview.$node.style.visibility = "";
+        oldWebview.$node.style.visibility = "";
         cb();
     });
-
 }
 
 
