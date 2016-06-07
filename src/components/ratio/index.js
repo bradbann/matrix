3ddc972971db37efb453531128be1facc0cdf377
伍峰杰 <wufengjie@u51.com>
2016-06-07 19:58:44 +0800

    update

--- src/components/ratio/index.js
+++ src/components/ratio/index.js
@@ -24,14 +24,14 @@ export default class Middle extends Component {
         if ( typeof this.template === 'function' ){
             return this.template();
         }
-        return `<div class="mx-ratio" role="ratio" :style="{'width':width}" :class="class"><div class="mx-ratio-inner"><slot></slot></div> <div class="mx-ratio-after" :style="{'padding-bottom':'100%'}"></div> </div>`;
+        return `<div class="mx-ratio" role="ratio" :style="{'width':width}" :class="class"><div class="mx-ratio-inner"><slot></slot></div> <div class="mx-ratio-after" :style="{'padding-bottom':value}"></div> </div>`;
     }
 
     _props(props){
         if ( !props ){ props = {} };
         props.width = {type:String,default:'100%'};
         props.align = String;
-        props.value = {type:String,default:'1'}
+        props.value = {type:String,default:"100%"}
 
         if ( typeof this.props === 'function' ){
             props = this.props(props);
