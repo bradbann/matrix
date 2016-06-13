0cbb43b1bdddc8e4558c24843ab5df0cc47bed85
evio <shenyunjie@51zhangdan.com.cn>
2016-06-13 12:35:36 +0800

    update

--- src/components/appview/appview.js
+++ src/components/appview/appview.js
@@ -6,10 +6,41 @@ export default class AppView extends Component {
         this.name = 'appview';
     }
 
+    _computed(computed){
+        if ( !computed ){
+            computed = {};
+        }
+
+        computed.style = function(){
+            if ( this.blank ){
+                return {
+                    'padding-top': 20
+                }
+            }
+        }
+
+        if ( this.computed ){
+            computed = this.computed(computed);
+        }
+
+        return computed;
+    }
+
     _template(){
         if ( typeof this.template === 'function' ){
             return this.template();
         }
-        return `<div class="mx-appview" role="appview"><slot></slot></div>`;
+        return `<div class="mx-appview" role="appview" :style="style"><slot></slot></div>`;
+    }
+
+    _props(props){
+        if ( !props ){ props = {} };
+        props.blank = Boolean;
+
+        if ( typeof this.props === 'function' ){
+            props = this.props(props);
+        }
+
+        return props;
     }
 }
