3959c4b79e3644d553533a6c277bd378388fdfde
evio <shenyunjie@51zhangdan.com.cn>
2016-06-14 00:15:09 +0800

    update

--- src/app/components.js
+++ src/app/components.js
@@ -1,15 +1,13 @@
-import { compile } from './util';
-import Container from '../components/container/index';
-import { Cells, Cell, Cell_head, Cell_body, Cell_foot } from '../components/cell/index';
-import { Panel, Panel_head, Panel_body, Panel_foot } from '../components/panel/index';
-import { Appview, Appview_head, Appview_body, Appview_foot } from '../components/appview/index';
-//import Icons from 'vue-icons';
-import { Flex, Flex_item } from '../components/flex/index';
 import Btn from '../components/button/index';
 import Middle from '../components/middle/index';
 import Aspect from '../components/aspect/index';
+import Container from '../components/container/index';
 
-// Icons.name = 'icon';
+import { compile } from './util';
+import { Flex, Flex_item } from '../components/flex/index';
+import { Panel, Panel_head, Panel_body, Panel_foot } from '../components/panel/index';
+import { Cells, Cell, Cell_head, Cell_body, Cell_foot } from '../components/cell/index';
+import { Appview, Appview_head, Appview_body, Appview_foot } from '../components/appview/index';
 
 const COMPONENTS = {
     // middle
@@ -20,9 +18,6 @@ const COMPONENTS = {
 
     //container
     "container"     : Container,
-
-    // icon
-    // "icon"          : Icons,
 
     // cell
     "cells"         : Cells,
