c8749c66718a18de8942ab1f7742412c8b843bcc
伍峰杰 <wufengjie@u51.com>
2016-06-15 19:09:32 +0800

    update

--- src/components/checkbox/index.js
+++ src/components/checkbox/index.js
@@ -9,7 +9,7 @@ export default class Checkbox extends Component {
         if ( typeof this.template === 'function' ){
             return this.template();
         }
-        return `<label class="mx-checkbox" :type="type" role="input:checkbox" :for="id" ><input :id="id" type="checkbox" :name="name" :value="value" class="mx-checkbox-input"><span @click="elClick" class="mx-checkbox-vision"></span><slot><slot></label>`;
+        return `<label class="mx-checkbox" :type="type" role="input:checkbox" :for="id" ><input :checked="checked" :id="id" type="checkbox" :name="name" :value="value" class="mx-checkbox-input"><span @click="elClick" class="mx-checkbox-vision"></span><slot><slot></label>`;
     }
     _methods(methods){
         if(!methods) methods = {};
@@ -25,8 +25,15 @@ export default class Checkbox extends Component {
     }
 
     _props(props){
-        if ( !props ){ props = [] };
-        props.push('name', 'value','id','size','type');
+        if ( !props ){ props = {} };
+        props = {
+            name :String,
+            value:String,
+            id:String,
+            size:String,
+            type:String,
+            checked:Boolean
+        }
 
         if ( typeof this.props === 'function' ){
             props = this.props(props);
